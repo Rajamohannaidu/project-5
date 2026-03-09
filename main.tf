5e9a007f7c34568a88edd8114ea1e80df1b05e5a
Rajamohannaidu <rajamohan4urs@gmail.com>
2026-03-09 14:57:15 +0530

    first commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.5"
 
   backend "gcs" {
-    bucket = "manojsharma-terraform-state"
+    bucket = "rajamohan-tf-state-myproject"
     prefix = "project-5/state"
   }
 
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "google" {
-  project = "my-ever-first-project"
+  project = "my-ever-first-project-488313"
   region  = "us-central1"
 }
 
@@ -38,8 +38,8 @@ resource "google_compute_subnetwork" "main_subnet" {
 
 # --- GKE Cluster ---
 resource "google_container_cluster" "primary" {
-  name     = "gke-cluster"
-  location = "us-central1"   # regional cluster
+  name                = "gke-cluster"
+  location            = "us-central1" # regional cluster
   deletion_protection = false
 
   network    = google_compute_network.main_vpc.self_link
@@ -51,16 +51,16 @@ resource "google_container_cluster" "primary" {
 
 # Node pool with 2 nodes, spread across zones
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-nodes"
-  cluster    = google_container_cluster.primary.name
-  location   = google_container_cluster.primary.location
+  name     = "primary-nodes"
+  cluster  = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location
 
   node_count = 2
 
   node_config {
-    machine_type = "e2-small"   # smallest recommended for GKE nodes
+    machine_type = "e2-small" # smallest recommended for GKE nodes
     # Control disk type and size
-    disk_type    = "pd-ssd"   # or "pd-standard" for HDD
+    disk_type    = "pd-ssd" # or "pd-standard" for HDD
     disk_size_gb = 10       # smallest allowed size is 10 GB
 
     oauth_scopes = [
@@ -73,7 +73,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
 # --- Service Networking for Cloud SQL ---
 resource "google_project_service" "service_networking" {
-  project = "my-ever-first-project"
+  project = "my-ever-first-project-488313"
   service = "servicenetworking.googleapis.com"
 }
 
@@ -95,14 +95,14 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 
 # --- Cloud SQL Instance (HA / DR) ---
 resource "google_sql_database_instance" "postgres_instance" {
-  name              = "postgres-ha"
-  region            = "us-central1"
-  database_version  = "POSTGRES_14"
+  name                = "postgres-ha"
+  region              = "us-central1"
+  database_version    = "POSTGRES_14"
   deletion_protection = false
 
   settings {
-    tier              = "db-f1-micro"        # smallest tier (not free in HA mode)
-    availability_type = "REGIONAL"           # HA across zones
+    tier              = "db-f1-micro" # smallest tier (not free in HA mode)
+    availability_type = "REGIONAL"    # HA across zones
 
     ip_configuration {
       ipv4_enabled    = false
@@ -121,5 +121,5 @@ resource "google_sql_database" "default_db" {
 resource "google_sql_user" "db_user" {
   name     = "appuser"
   instance = google_sql_database_instance.postgres_instance.name
-  password = "StrongPassword123!"   # replace with secret manager
+  password = "StrongPassword123!" # replace with secret manager
 }
